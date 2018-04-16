89b443f41e17fb10b7b8325122eea27901930185
Rossnerr <metei1997vasile@gmail.com>
2018-04-16 12:46:26 +0300

    Add files via upload

---
+++ EDP_Lab.4/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿using System.Drawing;
+
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 21;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Speed : Medium";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(497, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Resume";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(497, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Pause";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Enabled = true;
+            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.nyancat;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 80);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Blue;
+            this.ClientSize = new System.Drawing.Size(584, 261);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "EDP_Lab.4";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ EDP_Lab.4/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private Graphics graphics;
+        private SolidBrush brush;
+        private List<Object> objects;
+        private int dimensionX;
+        private int dimensionY;
+
+        public Form1()
+        {
+            InitializeComponent();
+            DoubleBuffered = true;
+            objects = new List<Object>();
+            this.MouseWheel += from_MouseWheel;
+            dimensionX = 40;
+            dimensionY = 40;
+        }
+
+        private void from_MouseWheel(object sender, MouseEventArgs e)
+        {
+            if(e.Delta > 0)
+            {
+                if(this.timer1.Interval > 1)
+                {
+                    this.timer1.Interval-=20;
+                }
+                else
+                {
+                    MessageBox.Show("It is the max speed.");
+                }
+            }
+
+            if(e.Delta < 0)
+            {
+                if(this.timer1.Interval < 41)
+                {
+                    this.timer1.Interval+=20;
+                }
+                else
+                {
+                    MessageBox.Show("It is the min speed.");
+                }
+            }
+
+            switch(this.timer1.Interval)
+            {
+                case 1:
+                    this.label2.Text = "Speed: High";
+                    break;
+
+                case 21:
+                    this.label2.Text = "Speed: Medium";
+                    break;
+
+                case 41:
+                    this.label2.Text = "Speed: Low";
+                    break;
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach(var obj in objects)
+            {
+                e.Graphics.FillEllipse(obj.brush, obj.PosX, obj.PosY, obj.DimensionX, obj.DimensionY);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            foreach(var obj in objects)
+            {
+                obj.MoveObj();
+                Move_NyanCat();
+                Collision(obj);
+            }
+            Invalidate();
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            int speedX = 2, speedY = 2;
+            bool create = true;
+            foreach(var obj in objects)
+            {
+                if(Math.Abs(e.X - obj.PosX) <= obj.DimensionX && Math.Abs(e.Y - obj.PosY) <= obj.DimensionY)
+                {
+                    create = false;
+                    MessageBox.Show("You can not create a ball inside another ball");
+                }
+            }
+
+            if (e.X + dimensionX >= this.ClientSize.Width || e.Y + dimensionY >= this.ClientSize.Height)
+            {
+                create = false;
+                MessageBox.Show("You can not create a ball out of Form's Area");
+            }
+
+            double areaOfObject = 2 * Math.PI * Math.Pow((dimensionX / 2), 2);
+            double areaOfForm = this.ClientSize.Width * this.ClientSize.Height;
+            if(objects.Count * areaOfObject > areaOfForm / 6)
+            {
+                create = false;
+                MessageBox.Show("The window is too small, in order add more" +
+                                "\nobjects make it larger!");                                
+            }
+
+            if(create)
+            {
+                if(Object.count % 4 == 3)
+                {
+                    speedX = -2;
+                }
+                if(Object.count % 4 == 2)
+                {
+                    speedY = -2;
+                }
+                if (Object.count % 4 == 1)
+                {
+                    speedX = -2;
+                    speedY = -2;
+                }
+
+                objects.Add(new Object(e.X, e.Y, speedX, speedY, dimensionX, dimensionY));
+            }
+        }
+
+        public void Collision(Object obj)
+        {
+            bool collision = false;
+
+            if(obj.PosX + obj.DimensionX >= this.ClientSize.Width)
+            {
+                collision = true;
+                obj.MoveX = -obj.MoveX;
+                obj.ChangeColor();
+                System.Media.SystemSounds.Beep.Play();                
+            }
+            if(obj.PosX <= 0)
+            {
+                collision = true;
+                obj.MoveX = -obj.MoveX;
+                obj.ChangeColor();
+                System.Media.SystemSounds.Asterisk.Play();
+            }
+
+            if(obj.PosY + obj.DimensionY >= this.ClientSize.Height)
+            {
+                collision = true;
+                obj.MoveY = -obj.MoveY;
+                obj.ChangeColor();
+                this.BackColor = this.BackColor == Color.Blue ? this.BackColor = Color.Green : this.BackColor = Color.Blue;
+            }
+            if(obj.PosY <= 0)
+            {
+                collision = true;
+                obj.MoveY = -obj.MoveY;
+                obj.ChangeColor();
+                this.BackColor = this.BackColor == Color.Blue ? this.BackColor = Color.Green : this.BackColor = Color.Blue;
+            }
+
+            if (this.pictureBox1.Location.X >= this.ClientSize.Width)
+            {
+                collision = true;
+                this.pictureBox1.Enabled = false;
+            }
+
+            if(collision)
+            {
+                return;
+            }
+        }
+
+        public void Move_NyanCat()
+        {
+            this.pictureBox1.Location = new Point(this.pictureBox1.Location.X + 1, this.pictureBox1.Location.Y);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.B)
+            {
+                if(this.timer1.Enabled == true)
+                {
+                    this.timer1.Enabled = false;
+                }
+                else
+                {
+                    this.timer1.Enabled = true;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.timer1.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.timer1.Enabled = false;
+        }
+    }
+}
